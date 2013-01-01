39442d8422fa9350bda86eb8e1492573eadc3799
keethan <a_keetan@yahoo.co.uk>
2013-01-01 22:20:41 +0100

    ok
    ok

--- features/support/app_life_cycle_hooks.rb
+++ features/support/app_life_cycle_hooks.rb
@@ -3,8 +3,9 @@ require 'calabash-android/operations'
 include Calabash::Android::Operations
 
 Before do |scenario|
-  start_test_server_in_background
       $startTime = Time.now.to_f
+  start_test_server_in_background
+
 end
 
 After do |scenario|
